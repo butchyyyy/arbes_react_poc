2498c33c04bfa7c4f6e6a779ab1de7b6a9a7d64e
Milan Koníř <Konir.M@gmail.com>
2017-05-25 16:07:23 +0200

    Fix addBankAccount action

--- src/bankaccounts/BankAccountReducer.ts
+++ src/bankaccounts/BankAccountReducer.ts
@@ -7,8 +7,8 @@ const bankAccountReducer = handleActions<BankAccount[], BankAccount>({
   [ADD_BANK_ACCOUNT]: (state: BankAccount[], action: Action<BankAccount>): BankAccount[] => {
     const newId: number = state.reduce((maxId, paymentOrder) => Math.max(paymentOrder.id, maxId), -1) + 1
     return [
-      new BankAccount(newId, action.payload.accountNumber),
       ...state,
+      new BankAccount(newId, action.payload.accountNumber),
     ]
   },
 }, [])
