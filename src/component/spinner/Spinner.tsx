9655d87357dce417040d8f057ba8d07587ffbc52
butchyyyy <Konir.M@gmail.com>
2017-06-22 14:41:50 +0200

    Spinner cleanup Dont render unnecessary dom objects when spinner is not shown Less variables to eas up spinner size customization

--- src/component/spinner/Spinner.tsx
+++ src/component/spinner/Spinner.tsx
@@ -9,26 +9,23 @@ interface Props {
 export class Spinner extends React.Component<Props, void> {
 
   public render() {
-    const spinner = this.props.loading ? (
-        <div>
-          <div className={styles.mask}/>
-          <div className={this.props.loading ? styles.spinner : ""}>
-            <div className={styles.rect1}/>
-            <div className={styles.rect2}/>
-            <div className={styles.rect3}/>
-            <div className={styles.rect4}/>
-            <div className={styles.rect5}/>
-          </div>
-        </div>
-      ) : null
-    return (
-        <div>
-          {spinner}
-          <div className={this.props.loading ? styles.mask : ""}>
-            {this.props.children}
-          </div>
-        </div>
-    )
+    if (this.props.loading) {
+      return (
+          <div>
+            <div>
+              <div className={styles.spinner}>
+                <div className={styles.rect1}/>
+                <div className={styles.rect2}/>
+                <div className={styles.rect3}/>
+                <div className={styles.rect4}/>
+                <div className={styles.rect5}/>
+              </div>
+            </div>
+            <div className={styles.mask}>{this.props.children}</div>
+          </div>)
+    } else {
+      return <div>{this.props.children}</div>
+    }
   }
 
 }
